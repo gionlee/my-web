123ef05f103fd9f0a04b264c36a2e5057ea27d96
lizhen <li.zhen@netstars.co.jp>
2019-11-08 20:21:58 +0900

    增加pdf显示

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,28 +3,37 @@
     <div class="introduction">
       <div class="author">
         <div class="comment">/** </div>
-        <div class="comment">* <span class="title" >@Author : </span><span class="text">gionlee</span></div>
-        <div class="comment">* <span class="title" >@Date : </span><span class="text">2019-11-08 17:29:00</span></div>
-        <div class="comment">* <span class="title" >@Last Modified by : </span><span class="text">gionlee</span></div>
-        <div class="comment">* <span class="title" >@Last Modified time : </span><span class="text">2019-11-08 17:29:00</span></div>
+        <div class="comment">* <span class="title">@Author : </span><span class="text">gionlee</span></div>
+        <div class="comment">* <span class="title">@Date : </span><span class="text">2019-11-08 17:29:00</span></div>
+        <div class="comment">* <span class="title">@Last Modified by : </span><span class="text">gionlee</span></div>
+        <div class="comment">* <span class="title">@Last Modified time : </span><span class="text">2019-11-08
+            17:29:00</span></div>
         <div class="comment">*/</div>
       </div>
       <div class="menu">
         <div class="item">
-            { 
+          {
         </div>
-        <div class="item"><span class="key">github</span> : <a href="https://github.com/gionlee/" class="value">https://github.com/gionlee/ ,</a></div>
-        <div class="item"><span class="key">cnblogs</span> : <a href="https://www.cnblogs.com/gionlee/" class="value">https://www.cnblogs.com/gionlee/ ,</a></div>
-        <div class="item"><span class="key">about me</span> : <a href="https://github.com/gionlee/" class="value">https://www.gionlee.com/#/about ,</a></div>
+        <div class="item"><span class="key">github</span> : <a href="https://github.com/gionlee/"
+            class="value">https://github.com/gionlee/ ,</a></div>
+        <div class="item"><span class="key">cnblogs</span> : <a href="https://www.cnblogs.com/gionlee/"
+            class="value">https://www.cnblogs.com/gionlee/ ,</a></div>
+        <div class="item"><span class="key">about me</span> : <a href="https://github.com/gionlee/"
+            class="value">https://www.gionlee.com/#/about ,</a></div>
         <div class="item">
-        }
-            </div>
+          }
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 <script>
+  import pdf from 'vue-pdf'
   export default {
+    components: {
+      pdf
+    },
     data() {
       return {
 

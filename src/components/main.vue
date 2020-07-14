baa375aa9fbc0c114ad99918187bbd4f086ba886
lizhen <li.zhen@netstars.co.jp>
2020-07-14 17:47:40 +0900

    增加后台管理模板

--- src/components/main.vue
+++ src/components/main.vue
@@ -6,40 +6,42 @@
         <div class="comment">* <span class="title">@Author : </span><span class="text">gionlee</span></div>
         <div class="comment">* <span class="title">@Date : </span><span class="text">2019-11-08 17:29:00</span></div>
         <div class="comment">* <span class="title">@Last Modified by : </span><span class="text">gionlee</span></div>
-        <div class="comment">* <span class="title">@Last Modified time : </span><span class="text">2019-11-08
-            17:29:00</span></div>
+        <div class="comment">* <span class="title">@Last Modified time : </span><span class="text">2020-04-01
+            20:12:45</span></div>
         <div class="comment">*/</div>
       </div>
       <div class="menu">
         <div class="item">
           {
         </div>
-        <div class="item"><span class="key">github</span> : <a href="https://github.com/gionlee/"
+        <div class="item"><span class="key">github</span> : <a target="_blank" href="https://github.com/gionlee/"
             class="value">https://github.com/gionlee/ ,</a></div>
-        <div class="item"><span class="key">cnblogs</span> : <a href="https://www.cnblogs.com/gionlee/"
+        <div class="item"><span class="key">cnblogs</span> : <a target="_blank" href="https://www.cnblogs.com/gionlee/"
             class="value">https://www.cnblogs.com/gionlee/ ,</a></div>
-        <div class="item"><span class="key">about me</span> : <a href="https://github.com/gionlee/"
-            class="value">https://www.gionlee.com/#/about ,</a></div>
+        <div class="item"><span class="key">my resume</span> : <a target="_blank" href="http://www.gionlee.com/pdf/gionlee.pdf"
+            class="value">http://www.gionlee.com/pdf/gionlee.pdf,</a></div>
+         <div class="item"><span class="key">admin-template</span> : <a target="_blank" href="http://admin.gionlee.com/"
+            class="value">http://admin.gionlee.com/ ,</a></div>
         <div class="item">
           }
         </div>
       </div>
     </div>
-
+    <div class="record">
+      <a target="_blank" href="http://www.beian.miit.gov.cn/">冀ICP备19032023号-1</a>
+      
+    </div>
   </div>
 </template>
 <script>
-  import pdf from 'vue-pdf'
   export default {
-    components: {
-      pdf
-    },
     data() {
       return {
 
       };
     },
-    methods: {}
+    methods: {
+    }
   };
 
 </script>
